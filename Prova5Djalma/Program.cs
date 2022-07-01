df1a10a1be8fe908e5fdf4ec775c14eea578f2a6
foxca <foxca@LAPTOP-UTQBBQF9>
2022-07-01 20:07:56 -0300

    Adicionar arquivos de projeto.

---
+++ Prova5Djalma/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova5Djalma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NOME: Rita de Cassia Franca Faria      RA: 6322054
+            // 5 - Fazer um programa para encontrar todos os números pares entre 1 e 100.
+            int i;
+
+            for (i = 1; i <= 50; i++)
+            {
+                Console.WriteLine(i * 2);
+            }
+                Console.ReadLine();
+
+                Console.ReadKey();
+        }
+    }
+}
